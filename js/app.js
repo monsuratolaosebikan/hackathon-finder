833eb3908e1fd53498e34c947774d0a1270edb9a
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2015-09-29 22:01:13 -0400

    Add pan to new search location

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function initAutocomplete() {
+function initMapLoad() {
 	map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.360977, lng: -71.064238},
     zoom: 15,
@@ -9,82 +9,56 @@ function initAutocomplete() {
   var input = document.getElementById('location-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+}
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+function updateMap(location) {
+  getHackathons(location);
+  //TODO: Clear old markers
+  //pan to new location
+  var mygeocoder = new google.maps.Geocoder();
+  mygeocoder.geocode({'address' : location}, function(results, status){
+  //console.log( "latitude : " + results[0].geometry.location.lat() );
+  //console.log( "longitude : " + results[0].geometry.location.lng() );
+  var panPoint = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        map.panTo(panPoint)
+});
+  //TODO: add markers for each hackathon
+    //addMarkers(location, map);
 
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+}
 
-    if (places.length == 0) {
-      return;
-    }
-
-    //prints out the location entered in input box (use with eventbrite)
-    //console.log(document.getElementById("location-input").value);
-
-    $.getJSON('https://www.eventbriteapi.com/v3/events/search/?q=hackathon&location.address=' + document.getElementById("location-input").value + '&token=7QJYTPRIY6MZY2SFNIQW',function (data) {
-    	//console.log(data);
-    	hackathons = data.events;
-    	if (hackathons.length) {
-    	console.log(data.events[0].name.text);
-    }
-    else {
-    	console.log("There are no hackathons in that area");
-    }
-    });
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+function addMarkers(location, map) {
+  // Add marker at each hackathon location
+  var marker = new google.maps.Marker({
+    position: location,
+    label: 'H',
+    map: map
   });
 }
 
-var HackathonModel = function () {
+function getHackathons(location) {
+$.getJSON('https://www.eventbriteapi.com/v3/events/search/?q=hackathon&location.address=' + location + '&token=7QJYTPRIY6MZY2SFNIQW',function (data) {
+      hackathons = data.events;
+      if (hackathons.length) {
+      console.log(data.events[0].name.text);
+    }
+    else {
+      console.log("There are no hackathons in that area");
+    }
+    });
+}
+
+var HackathonViewModel = function () {
 	var self = this;
-	self.ScrolltoMap = function () {
-	   // animate
-	   $('html, body').animate({
-	       scrollTop: $('#map').offset().top
-	     }, 300, function(){
-	       // when done, add hash to url
-	       window.location.hash = "#map";
-		});
-	};
-};
+  self.loc = ko.observable();
+  self.findHackathons = function() {
+    //TODO: Check if inputbox is empty
+    updateMap(self.loc());
+  };
+
+}
+
+$(document).ready(function() {
+  ko.applyBindings(new HackathonViewModel());
+});
 
